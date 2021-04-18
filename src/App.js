01c912b783f12108407baf277f74e43ab369bd2b
Amita <panwar.amita@gmail.com>
2021-04-17 20:40:37 -0400

    updated the package.json

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
     <Router>
     <div className="App">
         <Header/>
-        <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/#/" component={About} />
+        <Route exact path="/#/about" component={About} />
+        <Route exact path="/#/portfolio" component={Portfolio} />
+        <Route exact path="/#/contact" component={Contact} />
     </div>
     </Router>
   );
--- src/component/header.js
+++ src/component/header.js
@@ -10,13 +10,13 @@ return(
             <div className="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/contact">Contact</a>
+                        <a className="nav-link" href="/#/contact">Contact</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/portfolio">Portfolio</a>
+                        <a className="nav-link" href="/#/portfolio">Portfolio</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" href="/about">About</a>
+                        <a className="nav-link active" href="/#/about">About</a>
                     </li>
                 </ul>
             </div>
