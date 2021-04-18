b31cdccdc3034e921d91e47c595c1a719b80aa85
Amita <panwar.amita@gmail.com>
2021-04-17 22:50:29 -0400

    updated package file

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import {HashRouter as Router,Route }  from 'react-router-dom';
+import Header from "./component/header"
+import About from "./component/about";
+import Portfolio from "./component/potfolio";
+import Contact from "./component/contact";
+import Footer from "./component/footer";
+function App() {
+  return (
+    <Router>
+    <div className="App">
+        <Header/>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+       <Footer/>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
