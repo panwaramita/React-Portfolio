ba4b826303c9304d3c7f8e83a663a5f3178d7fe7
Amita <panwar.amita@gmail.com>
2021-04-17 20:49:08 -0400

    updated the package.json

--- src/component/header.js
+++ src/component/header.js
@@ -3,20 +3,20 @@ import '../css/style.css';
 const Header=()=>{
 return(
         <nav className="navbar header fixed-top navbar-expand-lg  navbar-light">
-            <a className="name-background" href="/">Amita</a>
+            <a className="name-background" href="#/">Amita</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="/#/contact">Contact</a>
+                        <a className="nav-link" href="#/contact">Contact</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/#/portfolio">Portfolio</a>
+                        <a className="nav-link" href="#/portfolio">Portfolio</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" href="/#/about">About</a>
+                        <a className="nav-link active" href="#/about">About</a>
                     </li>
                 </ul>
             </div>
