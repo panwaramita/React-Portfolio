9a451b5c23beff6c8898186b4590599cf12c25c1
Amita <panwar.amita@gmail.com>
2021-04-17 22:02:09 -0400

    updated header file

--- src/component/header.js
+++ src/component/header.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../css/style.css';
 const Header=()=>{
 return(
         <nav className="navbar header fixed-top navbar-expand-lg  navbar-light">
-            <a className="name-background" href="#/">Amita</a>
+             <Link to="/" className="name-background">
+                Amita
+              </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="#/contact">Contact</a>
+                    <Link to="/contact" className="nav-link h5 mb-0 link">
+                Contact
+              </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#/portfolio">Portfolio</a>
+                    <Link to="/portfolio" className="nav-link h5 mb-0 link">
+                Portfolio
+              </Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link active" href="#/about">About</a>
+                    <Link to="/about" className="nav-link h5 mb-0 link">
+                About
+              </Link>
                     </li>
                 </ul>
             </div>
